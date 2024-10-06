c2b122cd706bc5c86502af80c4c0492a5559bac6
Barac Fabregat <calmestend@gmail.com>
2024-10-05 18:55:02 -0600

    hotfix

--- nasa-web/src/routes/+page.svelte
+++ nasa-web/src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <h1>home</h1>
 
-<iframe title="map" src="http://127.0.0.1:5000/static/co2_map.html" frameborder="0"></iframe>
+<h2>Cero CO2: Hacia un Futuro Sostenible</h2>
+
+<!-- <iframe title="map" src="http://127.0.0.1:5000/static/co2_map.html" frameborder="0"></iframe> -->
